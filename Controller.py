3acaef7abedb83a58cff2269171f0bfa43868749
dkspindola <davi@spindola.de>
2021-07-05 11:40:03 +0200

    Trying to implement a pid controller to verify quadcopter dynamics.

--- Controller.py
+++ Controller.py
@@ -1,22 +1,31 @@
 import numpy as np
 
-from MathUtils import calc_angles
-from Config import config
+from MathUtils import calc_rot_matrix
+from numpy import cos
 
 
-class PD:
+class PID:
     def __init__(self):
         # controller gains
-        self.K_d = 4
-        self.K_p = 3
-        self.T = 0.005
-        self.mass = config.get("mass")
-        self.grav_acc = config.get("grav_acc")
-        self.inertia = config.get("inertia")
+        self.k_d = 4
+        self.k_p = 3
+        self.k_i = 5.5
 
-    def compute_action(self, obs):
-        rot_matrix = np.reshape(obs[6:15], (3, 3))
-        angles = np.array(calc_angles(rot_matrix))
+    def compute_action(self, env):
+        angles = env.angles
+        roll = angles[0]
+        pitch = angles[1]
+        yaw = angles[2]
+        rot_matrix = calc_rot_matrix(angles[0], angles[1], angles[2])
+        inertia = env.inertia
+
+        roll_err = self.k_d * roll_derivative + self.k_p *
+        pitch_err =
+        yaw_err =
+
+        thrust = env.mass * env.grav_acc[2] / (k * cos(roll) * cos(pitch))
+
+        actions = 1/4 * thrust + np.array([-1, 1, -1, 1]) * np.array([2 * yaw_err * inertia[0][0]])
 
         ang_vel = obs[15:]
 
