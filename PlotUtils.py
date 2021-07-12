c07a119d57fc143a8ea970704ca0f8f58882973e
dkspindola <davi@spindola.de>
2021-07-12 16:01:34 +0200

    Added information about environment. Some minor changes.

--- PlotUtils.py
+++ PlotUtils.py
@@ -3,12 +3,19 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from numpy import tanh
 
 from MathUtils import calc_angles
 
 
-def plot(actions, observations, env):
+def plot(actions, observations, env, reward):
+    """
+
+    :param actions:
+    :param observations:
+    :param env:
+    :param reward:
+    :return:
+    """
     # set font size
     mpl.rcParams['legend.fontsize'] = 10
     fig = plt.figure(dpi=100, edgecolor='black', facecolor='white')
@@ -22,9 +29,11 @@ def plot(actions, observations, env):
     ax.set_zlabel("z")
     ax.scatter(observations[0], observations[1], observations[2], color="grey")
     ax.text(observations[0], observations[1], observations[2], "start")
-    ax.scatter(env.goal_pos[0], env.goal_pos[1], env.goal_pos[2], color="green")
-    ax.text(env.goal_pos[0], env.goal_pos[1], env.goal_pos[2], "goal")
-    ax.plot(observations[0::18], observations[1::18], observations[2::18], label="position in [m]")
+    ax.scatter(0, 0, 0, color="green")
+    ax.text(0, 0, 0, "goal")
+    ax.quiver([observations[0]], [observations[1]], [observations[2]], observations[3], observations[4], observations[5],
+              length=np.linalg.norm([observations[3], observations[4], observations[5]]))
+    ax.plot(observations[0::18], observations[1::18], observations[2::18], color="tab:purple", label="position in [m]")
 
     ax = fig.add_subplot(3, 4, 2)
     ax.set_xlabel("time in [s]")
@@ -45,6 +54,24 @@ def plot(actions, observations, env):
     ax.plot(time, angles[2::3], label="yaw")
     ax.legend()
 
+    euclidean_distances = np.array([])
+    for i in range(0, len(observations), 18):
+        euclidean_distances = np.append(euclidean_distances, np.linalg.norm(observations[i:i+3]))
+
+    ax = fig.add_subplot(3, 4, 4)
+    ax.text(0, 1, "mean_pos_err: " + str(np.mean(euclidean_distances)) + "m")
+    ax.text(0, 0.9, "dt: " + str(env.dt) + "s")
+    ax.text(0, 0.8, "mass: " + str(env.mass) + "kg")
+    ax.text(0, 0.7, "arm_length: " + str(env.arm_length) + "m")
+    ax.text(0, 0.6, "settling_time: " + str(env.settling_time) + "s")
+    ax.text(0, 0.5, "thrust_to_weight: " + str(env.thrust_to_weight) + "kg/N")
+    ax.text(0, 0.4, "thrust_to_torque: " + str(env.thrust_to_torque) + "s^(-2)")
+    ax.text(0, 0.3, "start_pos_err: " + str(observations[:3]))
+    ax.text(0, 0.2, "start_lin_vel_err: " + str(observations[3:6]))
+    ax.text(0, 0.1, "start_ang_vel_err: " + str(observations[15:18]))
+    ax.text(0, 0, "episode_reward: " + str(reward))
+    ax.axis("off")
+
     ax = fig.add_subplot(3, 4, 5)
     ax.set_xlabel("time in [s]")
     ax.set_ylabel("angular velocity in [rad/s]")
@@ -59,7 +86,6 @@ def plot(actions, observations, env):
     ax.plot(time, observations[17::18], color="tab:green", label="d(gamma)/d(t)")
     ax.legend()
 
-    actions = tanh(actions)
     ax = fig.add_subplot(3, 4, 9)
     ax.set_xlabel("time in [s]")
     ax.set_ylabel("action in []")
